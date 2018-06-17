95748fe0b48905c163de190e150afede6a6e1f9e
Ishwer Kumar <ishwerkumar666@gmail.com>
2018-06-17 23:53:20 +0530

    Add Lexer Code and update Home

--- src/Compiler/Home.java
+++ src/Compiler/Home.java
@@ -4,7 +4,14 @@
  * and open the template in the editor.
  */
 package Compiler;
-
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
 /**
  *
  * @author joker
@@ -16,6 +23,7 @@ public class Home extends javax.swing.JFrame {
      */
     public Home() {
         initComponents();
+        this.initialize();
     }
 
     /**
@@ -27,22 +35,136 @@ public class Home extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton1 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jButton2 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jButton1.setText("Browse");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("File Path :");
+
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("Lexical Analysis");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jScrollPane1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabel1)
+                        .addGap(18, 18, 18)
+                        .addComponent(jTextField1)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton1)))
+                .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addGap(196, 196, 196)
+                .addComponent(jButton2)
+                .addContainerGap(196, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(22, 22, 22)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1))
+                .addGap(18, 18, 18)
+                .addComponent(jButton2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 299, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        JFileChooser jf = new JFileChooser();
+        jf.setAcceptAllFileFilterUsed(false);
+        jf.setApproveButtonText("Select");
+        jf.setMultiSelectionEnabled(false);
+        FileFilter filter = new FileNameExtensionFilter(".tiny File", "tiny");
+        jf.setFileFilter(filter);
+        int result = jf.showOpenDialog(this);
+        if(result == JFileChooser.APPROVE_OPTION) {
+            File f  = jf.getSelectedFile();
+            String gpath = f.getPath().replace("\\", "\\\\");
+                jTextField1.setText(gpath);
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        Lexer L = new Lexer(jTextField1.getText());
+        ArrayList<Token> Tokens;
+        Tokens = L.generateTokens();
+        for(Token T: Tokens){
+            jTextArea1.setText(jTextArea1.getText() + T.tokenID + "\t" + T.instance + "\t" + T.lineNum + "\t" + T.tokenType + "\n");
+        }
+    }//GEN-LAST:event_jButton2ActionPerformed
+    
+    private void initialize() {
+        /*JTextField T1;
+        JLabel L1;
+        JButton B1;
+        
+        T1 = new JTextField();
+        T1.setBounds(120, 30, 200, 20);
+        this.add(T1);
+        L1 = new JLabel();
+        L1.setText("Enter Path :");
+        L1.setBounds(20,30,100,20);
+        this.add(L1);
+        B1 = new JButton();
+        B1.setText("OK");
+        B1.setBounds(260,60,60,25);
+        this.add(B1);
+        */
+        this.setVisible(true);
+        this.setTitle("Compiler for Tiny");
+        this.setLocationRelativeTo(null);
+        String temp = String.valueOf(getClass().getResource("/RequiredFiles/Code.tiny"));
+        temp = temp.substring(5,temp.length());
+        temp = temp.replaceAll("%20", " ");
+        jTextField1.setText(temp);
+    }
     /**
      * @param args the command line arguments
      */
@@ -73,11 +195,17 @@ public class Home extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Home().setVisible(true);
+                new Home();
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
 }
--- src/Compiler/Lexer.java
+++ src/Compiler/Lexer.java
@@ -5,10 +5,237 @@
  */
 package Compiler;
 
+import java.awt.HeadlessException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author joker
  */
-public class Lexer {
+//  This is the Lexical Analyzer for Tiny Language.
+//  Definition of Tiny :: 
+
+//  Keywords                       :  	WRITE READ IF ELSE RETURN BEGIN END MAIN STRING INT REAL
+//  Single-character separators    :   	;  ,  (   )
+//  Single-character operators     :   	+  -  *   /
+//  Multi-character operators      :   	:=  ==   !=
+//  Identifier: An identifier consists of a letter followed by any number of letters or digits. The following are examples of identifiers: x, x2, xx2, x2x, End, END2.Note that End is an identifier while END is a keyword. The following are not identifiers:
+
+/*    IF, WRITE, READ, .... (keywords are not counted as identifiers)
+      2x (identifier can not start with a digit)
+      Strings in comments are not identifiers. */
+
+//  Number is a sequence of digits, or a sequence of digits followed by a dot, and followed by digits.   
+
+/*    Number -> Digits | Digits '.' Digits
+      Digits -> Digit | Digit Digits
+      Digit  -> '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' */
+
+//  Comments: string between /** and **/. Comments can be longer than one line.
+
+enum Type {
+    KEYWORD,
+    IDENTIFIER,
+    STRING_LITERAL,
+    NUMBER,
+    DELIMITER,
+    OPERATOR,
+    UNDEF,
+    EOT
+}
+
+class Token {
+    final int tokenID;
+    final int lineNum;
+    final String instance;
+    final Type tokenType;
     
+    public Token(int tokenID,int lineNum,String instance,Type tokenType){
+        this.tokenID = tokenID;
+        this.instance = instance;
+        this.tokenType = tokenType;
+        this.lineNum = lineNum;
+    }
+}
+
+
+public class Lexer {
+    String filePath;
+    String keywords[] = { "WRITE","READ","IF","ELSE","RETURN","BEGIN","END","MAIN","STRING","INT","REAL"};
+    int lineNum=1;
+    String multiCharOps[] = { ":=","==","!="};
+    char delimiter[] = {';',',','(',')'};
+    //char multiCharFirst[] = {':','=','!'};
+    char operators[] = {'+','-','*','/'};
+    ArrayList<String> words = new ArrayList();
+    //boolean comment=true;
+    
+    public Lexer(String filePath){
+        this.filePath = filePath;
+    }
+    
+    boolean isOperator(char c){
+        for(char i:operators)
+            if(c==i)
+                return true;
+        return false;
+    }
+    
+    boolean isMultiCharOp(char c, int i){
+        for(String s:multiCharOps)
+            if(s.charAt(0)==c && i==0)
+                return true;
+            else if(s.charAt(1)==c && i==1)
+                return true;
+        return false;
+    }
+    
+    boolean isDelimiter(char c){
+        for(char i:delimiter)
+            if(c==i)
+                return true;
+        return false;
+    }
+    
+    boolean isKeyword(String s){
+        for(String d:keywords)
+            if(d.equals(s))
+                return true;
+        return false;
+    }
+    
+    public ArrayList<Token> generateTokens(){
+        ArrayList<Token> Tokens;
+        Tokens = new ArrayList();
+        int tid = 0;
+        //FileReader fr = new FileReader(this.filePath);
+        int i;
+        char c;
+        try {
+        RandomAccessFile fr = new RandomAccessFile(filePath,"r");
+        long length = fr.length();
+        while(fr.getFilePointer()<length-1) {
+            c = (char)fr.read();            
+            //To check whether It's a comment or not
+            if(c == '/'){
+                c = (char)fr.read();
+                if(c == '*'){
+                    c = (char)fr.read();
+                    if(c == '*'){
+                        //JOptionPane.showMessageDialog(null, "It's a comment!!","Good News",JOptionPane.INFORMATION_MESSAGE);
+                        while((i=fr.read()) != -1){
+                            if((char)i == '*'){
+                                c = (char)fr.read();
+                                if(c == '*'){
+                                    c = (char)fr.read();
+                                    if(c == '/'){
+                                        //JOptionPane.showMessageDialog(null, "Commment Ended!!",Awesome!,JOptionPane.INFORMATION_MESSAGE);                             
+                                        break;
+                                    }    
+                                    else if(c == '\n')
+                                        lineNum++;
+                                }
+                                else if(c == '\n')
+                                    lineNum++;
+                            }
+                            else if(c == '\n')
+                                lineNum++;
+                        }
+                        continue;
+                    }
+                    else{
+                        fr.seek(fr.getFilePointer()-3);
+                        continue;
+                    }
+                }
+                else {
+                    fr.seek(fr.getFilePointer()-2);
+                    continue;
+                }
+            }
+            if(c == '\n') {
+		lineNum++;
+                continue;
+            }
+            
+            if(isOperator(c)) {
+                Tokens.add(new Token(++tid, lineNum, Character.toString(c), Type.OPERATOR));
+                continue;
+            }
+            
+            if(isMultiCharOp(c,0)){
+                String temp = Character.toString(c);
+                c = (char)fr.read();
+                if(isMultiCharOp(c, 1)){
+                    temp = temp + Character.toString(c);
+                    Tokens.add(new Token(++tid, lineNum, temp, Type.OPERATOR));
+                }
+                else {
+                    //fr.seek(fr.getFilePointer()-2);
+                    JOptionPane.showMessageDialog(null, "Error!\nOperators is fucked! at line " + Integer.toString(lineNum), "Error!", JOptionPane.ERROR_MESSAGE);
+                    //continue;
+                }
+            }
+            
+            if(isDelimiter(c)){
+                Tokens.add(new Token(++tid, lineNum, Character.toString(c), Type.DELIMITER));
+                continue;
+            }
+            
+            if(c == '"'){
+                String temp="";
+                c = (char)fr.read();
+                while(c != '"'){
+                    temp = temp + Character.toString(c);
+                    c = (char)fr.read();
+                }
+                Tokens.add(new Token(++tid, lineNum, temp, Type.STRING_LITERAL));
+                continue;
+            }
+            
+            if(Character.isAlphabetic(c)){
+                String temp = Character.toString(c);
+                c = (char)fr.read();
+                while(Character.isAlphabetic(c) || Character.isDigit(c)) {
+                    temp = temp + Character.toString(c);
+                    c = (char)fr.read();
+                }
+                //System.out.println(temp);
+                fr.seek(fr.getFilePointer()-1);
+                if(isKeyword(temp))
+                    Tokens.add(new Token(++tid, lineNum, temp, Type.KEYWORD));
+                else 
+                    Tokens.add(new Token(++tid, lineNum, temp, Type.IDENTIFIER));
+                continue;
+            }
+            if(Character.isDigit(c)){
+                String temp = Character.toString(c);
+                c = (char)fr.read();
+                while(Character.isDigit(c) || c == '.'){
+                    temp = temp + Character.toString(c);
+                    c = (char)fr.read();
+                }
+                //System.out.println(temp);
+                fr.seek(fr.getFilePointer()-1);
+                Tokens.add(new Token(++tid, lineNum, temp, Type.NUMBER));
+                continue;
+            }
+            if(c != ' ' && c != '\t'){
+                Tokens.add(new Token(++tid, lineNum, Character.toString(c), Type.UNDEF));
+                continue;
+            }
+        }
+        }
+        catch(HeadlessException | IOException e) {
+            JOptionPane.showMessageDialog(null, e,"Error!",JOptionPane.ERROR_MESSAGE);
+        }
+        Tokens.add(new Token(++tid, lineNum, null, Type.EOT));
+        return Tokens;
+    }
 }
